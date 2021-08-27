e550045d2e499c75692c32b03ce483013cded19e
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-08-27 17:26:26 +0300

    generating profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilesComponent
   ],
   imports: [
     BrowserModule
